ec54607054c2eb1c807adc72ac5b983c1065b785
seungtaekhong <hsk4991149@naver.com>
2024-08-16 09:19:44 +0900

    initialize project

---
+++ src/main/java/site/ithinkso/file_sharing_system/FileSharingSystemApplication.java
@@ -0,0 +1,13 @@
+package site.ithinkso.file_sharing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileSharingSystemApplication {
+
+	public static void main(String[] args)	 {
+		SpringApplication.run(FileSharingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/ithinkso/file_sharing_system/FileSharingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package site.ithinkso.file_sharing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileSharingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
