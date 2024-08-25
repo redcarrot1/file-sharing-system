d8fd47bd5ae7adf785d31909c0a51eb5f9efeacf
seungtaekhong <hsk4991149@naver.com>
2024-08-25 23:27:50 +0900

    test: Set SpringBootTest active profile

--- src/test/java/site/ithinkso/file_sharing_system/FileSharingSystemApplicationTests.java
+++ src/test/java/site/ithinkso/file_sharing_system/FileSharingSystemApplicationTests.java
@@ -2,12 +2,14 @@ package site.ithinkso.file_sharing_system;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FileSharingSystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
