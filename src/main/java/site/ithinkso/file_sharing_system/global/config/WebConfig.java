2104a76b459502450959d037d0e6f6f4999e0b1b
seungtaekhong <hsk4991149@naver.com>
2024-08-26 00:06:55 +0900

    update favicon

--- src/main/java/site/ithinkso/file_sharing_system/global/config/WebConfig.java
+++ src/main/java/site/ithinkso/file_sharing_system/global/config/WebConfig.java
@@ -12,6 +12,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoginCheckInterceptor())
                 .addPathPatterns("/**")
-                .excludePathPatterns("/login", "/logout", "/css/**", "/*.ico", "/error", "/favicon.webp");
+                .excludePathPatterns("/login", "/logout", "/css/**", "/*.ico", "/error", "/favicon.png");
     }
 }
